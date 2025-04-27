16cb3ca4b7c770e6f1b50f814d161e5fab55bbce
masashimorita <masashi.morita.mm@gmail.com>
2025-04-27 13:56:16 +0900

    :sparkles: Implemented to answer question with OpenAI based on documents

--- app.py
+++ app.py
@@ -1,4 +1,32 @@
 import streamlit as st
+from langchain import hub
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from utils.vector_store import (
+  create_vector_store, 
+  get_collection,
+  format_docs
+)
+from utils.llm import model
+
+
+def generate_answer(question):
+  if question:
+    try:
+      prompt = hub.pull("rlm/rag-prompt")
+      chain = (
+        {
+          "context": create_vector_store().as_retriever() | format_docs,
+          "question": RunnablePassthrough(),
+        }
+        | prompt
+        | model
+        | StrOutputParser()
+      )
+      return chain.invoke(question)
+    except Exception as e:
+      st.error(f"Error occurred while generation answer: {e}")
+      return None
 
 
 def manage_db():
@@ -7,6 +35,17 @@ def manage_db():
 
 def ask_question():
   st.header("Ask question about the documents")
+
+  question = st.text_input('Question:', placeholder='Input a brief summary of question')
+
+  if st.button('Submit') and question:
+    with st.spinner('Generating answer...'):
+      answer = generate_answer(question)
+      if answer:
+        st.success("回答:")
+        st.info(answer)
+      else:
+        st.error("Failed to generate answer...")
 
 
 def main():
