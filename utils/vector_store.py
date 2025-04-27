239d5864777a15c94320e49ad6aeff9d3ccb7d71
masashimorita <masashi.morita.mm@gmail.com>
2025-04-27 14:25:47 +0900

    :bug: Fix how to get / create a persisted chroma collection

--- utils/vector_store.py
+++ utils/vector_store.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
+import chromadb
 from langchain_chroma import Chroma
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from .llm import embeddings
 
 
@@ -21,7 +24,7 @@ def create_vector_store():
   return vector_store
 
 def get_collection():
-  client = Chroma.PersistentClient(path=DB_PERSIST_DIRECTORY)
+  client = chromadb.PersistentClient(path=DB_PERSIST_DIRECTORY)
 
   try:
     return client.get_collection(name=DB_COLLECTION_NAME)
@@ -31,3 +34,31 @@ def get_collection():
 
 def format_docs(docs):
   return "\n\n".join(doc.page_content for doc in docs)
+
+
+def get_documents_from_file(file_path: str, source: str):
+  loader = TextLoader(file_path)
+  raw_documents = loader.load()
+
+  for document in raw_documents:
+    document.metadata["source"] = source
+
+  text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=512,
+    chunk_overlap=100,
+    add_start_index=True,
+    separators=["\n\n", "\n", ".", " ", ""],
+  )
+
+  documents = text_splitter.split_documents(raw_documents)
+
+  original_ids = []
+  for doc in documents:
+    _source = os.path.splitext(os.path.basename(doc.metadata["source"]))[0]
+    _start = doc.metadata["start_index"]
+    original_ids.append(f"{_source}-{_start:08}")
+  
+  return {
+    "original_ids": original_ids,
+    "documents": documents,
+  }
